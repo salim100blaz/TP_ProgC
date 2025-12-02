90dcb2e3094cd1d1d6163921ad921bb2d6084691
salim100blaz <bourhimsalim22@gmail.com>
2025-12-02 09:36:06 +0100

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int num1 = 20;
+    int num2 = 6;
+    char op = '&';  
+
+    int resultat;
+
+    switch (op) {
+        case '+':
+            resultat = num1 + num2;
+            break;
+        case '-':
+            resultat = num1 - num2;
+            break;
+        case '*':
+            resultat = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                resultat = num1 / num2;
+            else {
+                printf("Erreur : division par 0\n");
+                return 1;
+            }
+            break;
+        case '%':
+            resultat = num1 % num2;
+            break;
+        case '&':
+            resultat = num1 & num2;
+            break;
+        case '|':
+            resultat = num1 | num2;
+            break;
+        case '~':
+            resultat = ~num1;
+            break;
+        default:
+            printf("Operateur invalide.\n");
+            return 1;
+    }
+
+    printf("Résultat = %d\n", resultat);
+    return 0;
+}
+
+
 
