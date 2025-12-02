1581a74808b7078cdfc63dd90e2a051a36eaefa0
salim100blaz <bourhimsalim22@gmail.com>
2025-12-02 09:32:52 +0100

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int compteur = 5;
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+#include <stdio.h>
+
+int main() {
+    int compteur = 5;
+    int i = 1;
+    while (i <= compteur) {
+        int j = 1;
+        while (j <= i) {
+            printf("* ");
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
