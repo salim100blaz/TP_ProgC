b5647f158b1fba3fc2b239c23d3076d91cfdd731
salim100blaz <bourhimsalim22@gmail.com>
2025-12-02 09:36:57 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <string.h>
 
+
+int main() {
+    int n = 5;
+
+    for (int i = 1; i <= n; i++) {
+
+        
+        for (int j = 0; j < n - i; j++) {
+            printf(" ");
+        }
+
+        
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("Pyramide terminée.\n");
+
+    return 0;
+}
